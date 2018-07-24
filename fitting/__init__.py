d7b209cec38e198b011f8ac65ac4374ec2bf1afe
Derek Fujimoto <fujimoto@phas.ubc.ca>
2018-07-24 10:18:25 -0700

    removed unnecessary imports

--- fitting/__init__.py
+++ fitting/__init__.py
@@ -1,7 +1,9 @@
 from bfit.fitting.ffreq import ffreq
-from bfit.fitting.fpulsed import fpulsed
 
 try:
     from bfit.fitting.integrator import PulsedFns
 except ImportError:
-    pass
+    print("\n integrator not compiled. From withing bfit/fitting/, call: "+\
+          "'python3 setup_integrator.py build_ext --inplace'")
+else:
+    from bfit.fitting.fpulsed import fpulsed
