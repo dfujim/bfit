2f1c8b0ab08a41696fd83139c46106faf3116aa9
Derek Fujimoto <fujimoto@phas.ubc.ca>
2019-09-16 13:55:48 -0700

    popup_fit_constraints: now displays and sets default inputs

--- bfit/gui/popup_fit_constraints.py
+++ bfit/gui/popup_fit_constraints.py
@@ -8,6 +8,7 @@ from functools import partial
 
 import logging,re
 import numpy as np
+import pandas as pd
 import weakref as wref
 
 from bfit import logger_name
@@ -27,15 +28,26 @@ class popup_fit_constraints(object):
         logger
         
         constr_text:        string, text defining constraints
-        new_par:            list, parameter names of new, shared, inputs
-        reserved_pars:      dict, define values in bdata that can be accessed
+        new_par:            dataframe, index: parnames, columns: p0,blo,bhi,res,err
+        
+        output_par_label    label, detected parameter names
+        output_text         dict, keys: p0,blo,bhi,res,err, value: tkk.Text objects
+       
         parnames:           list, function inputs
+        reserved_pars:      dict, define values in bdata that can be accessed
         win:                Toplevel
         
     """
 
     # names of modules the constraints have access to
     modules = {'np':'numpy'}
+    
+    # default parameter values on new parameter
+    default_parvals = { 'p0':1,
+                        'blo':-np.inf,
+                        'bhi':np.inf,
+                        'res':np.nan,
+                        'err':np.nan}
 
     # ====================================================================== #
     def __init__(self,bfit,constr_text=''):
@@ -51,13 +63,15 @@ class popup_fit_constraints(object):
         self.win = Toplevel(bfit.mainframe)
         self.win.title('Constrain Fit Parameters')
         frame = ttk.Frame(self.win,relief='sunken',pad=5)
+        left_frame = ttk.Frame(frame)
+        right_frame = ttk.Frame(frame)
 
         # Key bindings
         self.win.bind('<Return>',self.do_fit)             
         self.win.bind('<KP_Enter>',self.do_fit)
         
         # Keyword parameters
-        key_param_frame = ttk.Frame(frame,relief='sunken',pad=5)
+        key_param_frame = ttk.Frame(left_frame,relief='sunken',pad=5)
         s = 'Reserved variable names:\n\n'
         self.reserved_pars = CstrFn.ConstrainedFunction.keyvars
         
@@ -70,7 +84,7 @@ class popup_fit_constraints(object):
         key_param_label = ttk.Label(key_param_frame,text=s,justify=LEFT)
         
         # fit parameter names 
-        fit_param_frame = ttk.Frame(frame,relief='sunken',pad=5)
+        fit_param_frame = ttk.Frame(left_frame,relief='sunken',pad=5)
         s = 'Reserved function input names:\n\n'
         self.parnames = self.fittab.fitter.gen_param_names(
                                         self.fittab.fit_function_title.get(),
@@ -80,7 +94,7 @@ class popup_fit_constraints(object):
         fit_param_label = ttk.Label(fit_param_frame,text=s,justify=LEFT)
 
         # module names 
-        module_frame = ttk.Frame(frame,relief='sunken',pad=5)
+        module_frame = ttk.Frame(left_frame,relief='sunken',pad=5)
         s = 'Reserved module names:\n\n'
         
         keys = list(self.modules.keys())
@@ -92,7 +106,7 @@ class popup_fit_constraints(object):
         modules_label = ttk.Label(module_frame,text=s,justify=LEFT)
         
         # Text entry
-        entry_frame = ttk.Frame(frame,relief='sunken',pad=5)
+        entry_frame = ttk.Frame(right_frame,relief='sunken',pad=5)
         entry_label = ttk.Label(entry_frame,justify=LEFT,
                                 text='Enter one constraint per line.'+\
                                      '\nNon-reserved words are shared variables.'+\
@@ -104,14 +118,34 @@ class popup_fit_constraints(object):
         
         
         # fit
-        parse_button = ttk.Button(frame,text='Parse Input',command=self.do_parse)
+        parse_button = ttk.Button(right_frame,text='Parse Input',command=self.do_parse)
         
         
-        # ~ # text for output
-        # ~ self.model_results_text = Text(frame,width=17,height=8,state='normal')
-        # ~ self.model_errors_text = Text(frame,width=17,height=8,state='normal')
+        # text for output
+        output_frame = ttk.Frame(right_frame,relief='sunken',pad=5)
+        output_head1_label = ttk.Label(output_frame,text='Par Name')
+        output_head2_label = ttk.Label(output_frame,text='p0')
+        output_head3_label = ttk.Label(output_frame,text='Bounds')
+        output_head4_label = ttk.Label(output_frame,text='Result')
+        output_head5_label = ttk.Label(output_frame,text='Error')
+        self.output_par_label = ttk.Label(output_frame,justify=LEFT,text='    ')
+        self.output_text = {k:Text(output_frame,width=8,height=8,state='normal')\
+                            for k in ('p0','blo','bhi','res','err')}
+        for k in ('res','err'):
+            self.output_text[k].config(state='disabled',width=9)
+                
+        c = 0; r = 0;
+        output_head1_label.grid(column=c,row=r);        c+=1;
+        output_head2_label.grid(column=c,row=r);        c+=1;
+        output_head3_label.grid(column=c,row=r,
+                                columnspan=2);          c+=2;
+        output_head4_label.grid(column=c,row=r);        c+=1;
+        output_head5_label.grid(column=c,row=r);        c+=1;
         
-        # ~ self.model_results_text.insert('1.0',self.result_str)
+        c = 0; r += 1;
+        self.output_par_label   .grid(column=c,row=r,sticky=N); c+=1;
+        for k in ('p0','blo','bhi','res','err'):
+            self.output_text[k].grid(column=c,row=r,sticky=N); c+=1;
         
         # gridding
         key_param_label.grid(column=0,row=0)
@@ -123,13 +157,20 @@ class popup_fit_constraints(object):
         
         # grid to frame
         frame.grid(column=0,row=0)
+        left_frame.grid(column=0,row=0,sticky=(N,S))
+        right_frame.grid(column=1,row=0,sticky=(N,S))
         
         key_param_frame.grid(column=0,row=0,rowspan=1,sticky=(E,W),padx=1,pady=1)
-        module_frame.grid(column=0,row=1,sticky=(E,W),padx=1,pady=1)
-        fit_param_frame.grid(column=0,row=2,sticky=(E,W),padx=1,pady=1)
+        module_frame.grid(column=0,row=1,sticky=(E,W),padx=1,pady=1,rowspan=2)
+        fit_param_frame.grid(column=0,row=3,sticky=(E,W),padx=1,pady=1)
         
-        entry_frame.grid(column=1,row=0,sticky=(N),padx=1,pady=1)
-        parse_button.grid(column=1,row=1,sticky=(N,E,W),padx=1,pady=1)
+        entry_frame.grid(column=0,row=0,sticky=(N),padx=1,pady=1)
+        parse_button.grid(column=0,row=1,sticky=(N,E,W),padx=1,pady=1)
+        output_frame.grid(column=0,row=2,sticky=(N,E,W),padx=1,pady=1)
+        
+        # initialize 
+        self.new_par = pd.DataFrame(columns=['name','p0','blo','bhi','res','err']) 
+        
         
         self.logger.debug('Initialization success. Starting mainloop.')
     
@@ -151,6 +192,10 @@ class popup_fit_constraints(object):
         # clean input
         text = self.constr_text.split('\n')
         text = [t.strip() for t in text if '=' in t]
+        
+        # check for no input
+        if not text:
+            return
         
         # get equations and defined variables
         defined = [t.split('=')[0].strip() for t in text]
@@ -201,8 +246,45 @@ class popup_fit_constraints(object):
                 del lst[i]
                 
             new_par.append(lst)
-                
-        return (text,new_par)    
+
+        # add result
+        new_par = np.unique(np.concatenate(new_par))
+        for k in new_par:
+            
+            # bad input
+            if not k: continue
+            
+            # set defaults
+            if k not in self.new_par['name'].values:
+                self.new_par = self.new_par.append({'name':k,**self.default_parvals},
+                                                   ignore_index=True)
+        
+        # drop results 
+        for i,k in zip(self.new_par.index,self.new_par['name']):
+            if k not in new_par:
+                self.new_par.drop(i,inplace=True)
+        
+        # allow setting
+        for k in ('res','err'):
+            self.output_text[k].config(state='normal')
+
+        # set fields
+        self.new_par.sort_values('name',inplace=True)
+        set_par = self.new_par.astype(str)
+        self.output_par_label['text']   = '\n'.join(set_par['name'])
+        
+        for k in self.output_text:
+            self.output_text[k].delete('1.0',END)
+            self.output_text[k] .insert(1.0,'\n'.join(set_par[k]) )
+        
+        # disable setting
+        for k in ('res','err'):
+            self.output_text[k].config(state='normal')
+    
+        # logging
+        self.logger.info('Parse found constraints for %s, and defined %s',
+                         sorted(defined),
+                         set_par['name'].values.tolist())
     
     # ====================================================================== #
     def get_input(self,*args):
