70647cb6edc1b296b4b54a60e4d9d05ed1bbc515
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-09-27 19:16:06 -0700

    v3.5.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext = Extension("bfit.fitting.integrator",
 
 setuptools.setup(
     name="bfit",
-    version="3.4.2",
+    version="3.5.0",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="BNMR/BNQR Data Fitting and Visualization",
