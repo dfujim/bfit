fc2380bcbdd5bf32f551ee9d3e544200f47c9e5f
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-10-30 12:39:13 -0700

    Now allows direct import of fitting backend

--- bfit/__init__.py
+++ bfit/__init__.py
@@ -1,7 +1,12 @@
 import os
+import bfit.fitting.functions as functions
+import bfit.fitting.fit_bdata as fit_bdata
+import bfit.fitting.global_bdata_fitter as global_bdata_fitter
+import bfit.fitting.global_fitter as global_fitter
 
 __all__ = ['gui','fitting','backend']
 __version__ = '3.6.3'
 __author__ = 'Derek Fujimoto'
 logger_name = 'bfit'
 icon_path = os.path.join(os.path.dirname(__file__),'images','icon.gif')
+
