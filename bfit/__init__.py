8f40beb91198d79ee2ae5f36fa9f5731334bcacd
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-11-10 13:06:30 -0800

    v4.0.1

--- bfit/__init__.py
+++ bfit/__init__.py
@@ -6,7 +6,7 @@ from .fitting.global_bdata_fitter import global_bdata_fitter
 from .fitting.fit_bdata import fit_bdata
 
 __all__ = ['gui','fitting','backend']
-__version__ = '4.0.0'
+__version__ = '4.0.1'
 __author__ = 'Derek Fujimoto'
 logger_name = 'bfit'
 icon_path = os.path.join(os.path.dirname(__file__),'images','icon.gif')
