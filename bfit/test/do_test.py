f9b1e3c74a9320d8cffc808ec3960c64638035a1
Derek Fujimoto <fujimoto@phas.ubc.ca>
2021-02-03 17:27:30 -0800

    Added minuit testing and test_perfet

--- bfit/test/do_test.py
+++ bfit/test/do_test.py
@@ -1,5 +1,7 @@
 import bfit.test.functions as fn
 import bfit.test.leastsquares as ls
+import bfit.test.minuit as mnt
+
 
 # test functions
 # ~ fn.test_lorentzian()
@@ -10,14 +12,19 @@ import bfit.test.leastsquares as ls
 # ~ fn.test_pulsed_strexp()
 
 # test least squares
-ls.test_no_errors()
-ls.test_dy()
-ls.test_dx()
-ls.test_dxdy()
-ls.test_dya()
-ls.test_dxa()
-ls.test_dx_dya()
-ls.test_dxa_dy()
-ls.test_dxa_dya()
+# ~ ls.test_no_errors()
+# ~ ls.test_dy()
+# ~ ls.test_dx()
+# ~ ls.test_dxdy()
+# ~ ls.test_dya()
+# ~ ls.test_dxa()
+# ~ ls.test_dx_dya()
+# ~ ls.test_dxa_dy()
+# ~ ls.test_dxa_dya()
 
 
+mnt.test_start()
+mnt.test_name()
+mnt.test_error()
+mnt.test_limit()
+mnt.test_fix()
--- bfit/test/testing.py
+++ bfit/test/testing.py
@@ -8,6 +8,12 @@ def test(a, b, msg, tol=1e-9):
     else:
         raise AssertionError(msg + ": " + str(a) + ' != ' + str(b))    
 
+def test_perfect(a, b, msg):
+    if a == b:
+        print("Success: Tested %s" % msg)
+    else:
+        raise AssertionError(msg + ": " + str(a) + ' != ' + str(b))    
+
 def test_arr(a, b, msg, tol=1e-9):
     if all(abs(a-b) < tol):
         print("Success: Tested %s accurate to a tolerance of %g" % (msg, tol))
