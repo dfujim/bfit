a53994443bcf18fbee2db94545d9b824931d57bf
Derek Fujimoto <fujimoto@phas.ubc.ca>
2021-11-09 13:49:00 -0800

    v4.19.1

--- bfit/global_variables.py
+++ bfit/global_variables.py
@@ -6,7 +6,7 @@ from os.path import join
 
 # variables
 __src__ = join('bfit', 'fitting')
-__version__ = '4.19.0'
+__version__ = '4.19.1'
 
 # keywords used to identify variables
 KEYVARS = { 'B0'    : 'B0 Field (T)', 
