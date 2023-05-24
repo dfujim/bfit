d7072dbdfa0ad4a01f84be9db15634e39f0c673c
dfujimoto <dfujimoto@triumf.ca>
2023-05-24 15:02:37 -0700

    v4.20.4: updated to toml install

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 
 # install numpy and cython dependencies needed for the rest of the setup script
-from setuptools import dist
+from setuptools import dist, Extension, setup, find_packages
 dist.Distribution().fetch_build_eggs(['cython>=0.28', 'numpy>=1.19'])
 
-import setuptools
-from distutils.core import Extension
 from Cython.Build import cythonize
 import numpy, os, sys
 from os.path import join
@@ -16,7 +14,7 @@ with open("README.md", "r", encoding = "utf8") as fh:
 variables  =  {}
 with open(os.path.join('bfit', 'global_variables.py')) as fid:
     exec(fid.read(), variables)
-    
+
 __version__  =  variables['__version__']
 __src__  =  variables['__src__']
 
@@ -29,26 +27,26 @@ if sys.platform in ('unix', 'darwin'):
 ext  =  Extension("bfit.fitting.integrator",
                 sources = [join(__src__, "integrator.pyx"),
                            join(__src__, "FastNumericalIntegration_src", "integration_fns.cpp")],
-                language = "c++",             # generate C++ code                        
-                include_dirs = [join(__src__, "FastNumericalIntegration_src"), 
+                language = "c++",             # generate C++ code
+                include_dirs = [join(__src__, "FastNumericalIntegration_src"),
                                 numpy.get_include()],
                 libraries = libraries,
                 extra_compile_args = ["-ffast-math", '-O3']
                 )
 
-setuptools.setup(
+setup(
     name = "bfit",
     version = __version__,
     author = "Derek Fujimoto",
-    author_email = "fujimoto@phas.ubc.ca",
+    author_email = "dfujimoto@triumf.ca",
     description = "β-NMR and β-NQR Data Analysis",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/dfujim/bfit",
-    packages = setuptools.find_packages(),
+    packages = find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",     
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Cython",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
@@ -57,8 +55,8 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
     ],
     install_requires = ['cython >= 0.28', 'numpy >= 1.19', 'tqdm >= 4.25.0',
-                      'bdata >= 6.11.2', 'matplotlib >= 2.2.4', 'pandas >= 1.4.2',
-                      'pyyaml >= 5.1', 'scipy >= 1.2.0', 'iminuit >= 2.6.1', 
+                      'bdata >= 6.11.3', 'matplotlib >= 2.2.4', 'pandas >= 1.4.2',
+                      'pyyaml >= 5.1', 'scipy >= 1.2.0', 'iminuit >= 2.6.1',
                       'requests >= 2.25.0', 'argparse >= 1.4.0', 'pytest >= 4.5.0',
                       'wheel>=0.34', 'jax>=0.2.17', 'jaxlib>=0.1.69'],
     package_data = {'': ['data']},
